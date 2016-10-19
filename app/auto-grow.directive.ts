f5444ac552218596bbf597be7364b7a5c24e96f8
mshahpazova <maria.shahpazova@xoomworks.com>
2016-10-19 12:29:13 +0300

    Added two events - key-value pairs to the host object - focus in quotes and parenthesis with a method - on Focus to be implemented in the directive and event blur respectively

--- app/auto-grow.directive.ts
+++ app/auto-grow.directive.ts
@@ -2,7 +2,8 @@ import {Directive} from 'angular2/core';
 @Directive ({
     selector: '[autoGrow]',
     host: {
-        
+        '(focus)': 'onFocus',
+        '(blur)': 'onBlur()'
     }
 })
 export class AutoGrowDirective {
