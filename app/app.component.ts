9b923bb65abee1e24b4c7fb04bc4c64495990521
mshahpazova <maria.shahpazova@xoomworks.com>
2016-10-21 17:05:36 +0300

    Added favorite -component and included it in the app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,22 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
+import {FavoriteComponent} from './favorite.component'
 @Component({
     selector: 'my-app',
     template: `
+        <course></course>
+        <author></author>
         <input type="text" [value]="title" (input)="title = $event.target.value"/>
         <input type="button" (click)= "title = ''" value="Clear"/>
         <input type="text" [(ngModel)]="title"/>
         <input type="text" bindon-ngModel="title"/>
+        <favorite> </favorite>
         Preview: {{title}}
      `,
-    directives: [CoursesComponent, AuthorsComponent]
+    directives: [CoursesComponent, AuthorsComponent, FavoriteComponent]
 })
 export class AppComponent {
     title = "Angular App";
+
  } 
