1aad0dad94fabc8bff824179a34f0a6d7d87161a
mshahpazova <maria.shahpazova@xoomworks.com>
2016-10-21 11:53:52 +0300

    Added event handler that makes the connection from dom element to component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,19 +4,12 @@ import {AuthorsComponent} from './authors.component'
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{title}}</h1>
-    <course></course>
-    <author></author>
-    <button (click)="onClick($event)">Submit</button>
+        <input type="text" [value]="title" (input)="title = $event.target.value"/>
+        
+        Preview: {{title}}
      `,
     directives: [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent {
     title = "Angular App";
-    imageUrl = "http://lorempixel.com/400/200/"
-    isActive = false;
-
-    onClick($event){
-        console.log("Clicked");
-    }
  } 
