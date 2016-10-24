a3b660cb8c695ac414f68dd7c9dec992bb0d5dc5
mshahpazova <maria.shahpazova@xoomworks.com>
2016-10-24 15:39:10 +0300

    Deleted parameter of the @input decorator

--- app/favorite.component.ts
+++ app/favorite.component.ts
@@ -1,21 +1,20 @@
-import {Component, Input, EventEmitter } from 'angular2/core';
+import {Component, Input, Output, EventEmitter } from 'angular2/core';
 
 @Component({
     selector: 'favorite',
-    template: `
-    <i
-        class="glyphicon"
-        [class.glyphicon-star-empty]="!isFavorite"
-        [class.glyphicon-star]="isFavorite"
-        (click)="onClick()">
-        </i>
-    `,
+    templateUrl: 'app/favorite.template.html',
+    styles: [`
+    .glyphicon-star {
+        color: orange;
+    }
+    `]
 })
 export class FavoriteComponent {
-   @Input('is-favorite') isFavorite = false; 
+   @Input() isFavorite = false; 
 
-   change = new EventEmitter();
+   @Output() change = new EventEmitter();
     onClick(){
         this.isFavorite = !this.isFavorite;
+        this.change.emit({newValue: this.isFavorite})
     }
 }
