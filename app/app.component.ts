9b944ad4662fbf6551f58b9a106f2d43133dca40
mshahpazova <maria.shahpazova@xoomworks.com>
2016-10-24 18:40:35 +0300

    Handling the event when like button is clicked to increase likes of a certain post

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,21 +2,28 @@ import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
 import {FavoriteComponent} from './favorite.component'
+import {LikeComponent} from './like.component'
 @Component({
     selector: 'my-app',
     template: `
-        <i class="glyphicon glyphicon-star"></i>
-        <favorite [isFavorite]="post.isFavorite" (change)="onFavoriteChange($event)"> </favorite>
+        <like (change)="onLikeChange($event)"></like>
+        {{post.numberofLikes}}
      `,
-    directives: [FavoriteComponent]
+    directives: [FavoriteComponent, LikeComponent]
 })
 export class AppComponent {
     post = {
         title: "Title",
-        isFavorite: true
+        isFavorite: true,
+         isLiked: false,
+         numberofLikes: 5
     }
     onFavoriteChange($event){
         console.log($event);
     }
-
+       onLikeChange($event){
+           this.post.numberofLikes += $event.newValue === true ? 1 : -1;
  } 
+
+        //  <i class="glyphicon glyphicon-star"></i>
+        // <favorite [isFavorite]="post.isFavorite" (change)="onFavoriteChange($event)"> </favorite>
