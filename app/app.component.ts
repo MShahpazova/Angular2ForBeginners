4522ead65a5bf38d24e0f7ede6aa661a6fa3c917
MShahpazova <mshahpazova@gmail.com>
2016-12-12 15:03:23 +0200

    *ngFor Directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,17 +4,11 @@ import {ZippyComponent} from './zippy.component'
 @Component({
     selector: 'my-app',
     template: `
-     <ul class="nav nav-pills">
-     <!--bootstrap requires the li that represents the current tab to hve the class active-->
-       <li [class.active]="viewMode == 'map'"><a (click)="viewMode ='map'">Map view</a>
-       <li [class.active]="viewMode == 'list'"><a (click)="viewMode = 'list'">List view</a></li>
-     </ul>
-     <div [ngSwitch]="viewMode">
-       <template [ngSwitchWhen]="'map'" ngSwitchDefault> Map View Content<template>
-       <template [ngSwitchWhen]="'list'">List View Content</template>
-     </div>
+      <ul>
+        <li> *ngFor="#course of courses"></li>
+      </ul>
      `
 })
 export class AppComponent {
-  viewMode = 'map';
+  courses = ['Course 1', 'Course 2', 'Course 3']
 }
