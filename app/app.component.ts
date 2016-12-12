e67537e480cd50cb9c36492b2a7c569d74966d9f
MShahpazova <mshahpazova@gmail.com>
2016-12-12 14:59:38 +0200

    ngSwitch directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,18 @@ import {BootStrapPanel} from './bootstrap.panel.component';
 import {ZippyComponent} from './zippy.component'
 @Component({
     selector: 'my-app',
-    directives: [BootStrapPanel, ZippyComponent],
     template: `
-     <bs-panel>
-        <div class="heading">This is the heading</div>
-        
-        <div class="body">This is the body</div>
-      </bs-panel>
-      <zippy>Maria</zippy>
+     <ul class="nav nav-pills">
+     <!--bootstrap requires the li that represents the current tab to hve the class active-->
+       <li [class.active]="viewMode == 'map'"><a (click)="viewMode ='map'">Map view</a>
+       <li [class.active]="viewMode == 'list'"><a (click)="viewMode = 'list'">List view</a></li>
+     </ul>
+     <div [ngSwitch]="viewMode">
+       <template [ngSwitchWhen]="'map'" ngSwitchDefault> Map View Content<template>
+       <template [ngSwitchWhen]="'list'">List View Content</template>
+     </div>
      `
 })
 export class AppComponent {
+  viewMode = 'map';
 }
