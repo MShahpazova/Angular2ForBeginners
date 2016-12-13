a070f4a55a4d9f48a84eb7e97b1cc9f6548cc271
MShahpazova <mshahpazova@gmail.com>
2016-12-13 12:29:20 +0200

    created a form object, used it in a template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,30 @@
 import {Component} from 'angular2/core';
 import {BootStrapPanel} from './bootstrap.panel.component';
 import {ZippyComponent} from './zippy.component'
+import {ControlGroup, FormBuilder} from 'angular2/common';
 @Component({
     selector: 'my-app',
     template: `
+
       <ul>
-      <!--Leading aserisk tells angular to treat an element as a template-->
-        <li *ngFor="#course of courses, #i = index">
+       <li *ngFor="#course of courses, #i = index">
           {{i + 1}} -- {{course}}
         </li>
       </ul>
+     
+     <form [ngFormModel]="form">
+    <input type="text" ngControl="search">
+    </form>
+       
+
      `
 })
 export class AppComponent {
+  form: ControlGroup;
+  constructor(fb: FormBuilder){
+    this.form = fb.group({
+      search: []
+    })
+  }
   courses = ['Course 1', 'Course 2', 'Course 3']
 } 
