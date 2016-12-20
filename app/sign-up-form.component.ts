09b8fd55aba4115da43f6d432498402776644a2f
MShahpazova <mshahpazova@gmail.com>
2016-12-20 15:13:49 +0200

    Creating controls explicitly, using formbuilders

---
+++ app/sign-up-form.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+import {ControlGroup, Control, Validators, FormBuilder} from 'angular2/common';
+@Component({
+    selector: 'signup-form',
+    templateUrl: 'app/sign-up-form.component.html'
+})
+export class SignUpFormComponent {
+    form: ControlGroup;
+    constructor(fb: FormBuilder){
+        //creating a control-group object
+        this.form = fb.group({
+            username:['', Validators.required],
+            password:['', Validators.required]
+        })
+    }
+    // form = new ControlGroup({
+    //     //each key represents the name of the control in this group
+    //     username: new Control('', Validators.required),
+    //     password: new Control('', Validators.required)
+    // });
+    signup(){
+        // console.log(this.form.value)
+    }
+}
